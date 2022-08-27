ed36ccf986287f93c20ef08d00e51eaf5b9f1935
hasan923 <hassanbaig923@gmail.com>
2022-08-27 19:30:33 +0500

    home page routing

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,7 +1,12 @@
 import "./directory-item.styles.scss";
 
+import { useNavigate } from 'react-router-dom'
+
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler=() => navigate(route);
   return (
     <div className="directory-item-container">
       <div
@@ -10,7 +15,7 @@ const DirectoryItem = ({ category }) => {
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className="directory-item-body-container">
+      <div onClick={onNavigateHandler} className="directory-item-body-container">
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,28 +1,34 @@
-import { useParams } from 'react-router-dom';
-import { useContext, useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import { CategoriesContext } from "../../contexts/categories.context";
 
-import ProductCard from '../../components/product-card/product-card.component';
+import ProductCard from "../../components/product-card/product-card.component";
 
-import './category.styles.scss';
+import "./category.styles.scss";
 
 const Category = () => {
-    const { category } = useParams();
-    const { categoriesMap } = useContext(CategoriesContext)
-    const [products,setProduts] = useState(categoriesMap[category]);
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProduts] = useState(categoriesMap[category]);
 
-    useEffect(() => {
-        setProduts(categoriesMap[category]);
-    }, [category, categoriesMap])
-    
-    return (
-        <div className='category-container'>
-            {products &&
-            products.map((product)=><ProductCard key={product.id} product={product}/>)
-            }
-        </div>
-    )
-}
+  useEffect(() => {
+    setProduts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <>
+      <h2 className="category-title">
+        {category.charAt(0).toUpperCase() + category.slice(1)}
+      </h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </>
+  );
+};
 
 export default Category;
